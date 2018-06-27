f7f790da82c22686e5d2b16ce54b92a7bc4b1837
Jevin Fernando <jevinruv@gmail.com>
2018-06-27 19:00:01 +0530

    Created Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      [
+        { path: '', component: HomeComponent },
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
